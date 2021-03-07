68ea7e3a3ec1bea03e9582c27b7dd9f6e4490add
jwilson42 <wilson.jd022@gmail.com>
2021-03-07 14:13:51 -0800

    changed the path to the front-end

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json()); // JSON parsing
 app.use(cors()); // allow all CORS requests
 app.use(passport.initialize());
-app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+app.use(express.static(path.resolve(__dirname, 'clients', 'build')));
 
 // API Routes
 app.get('/api/', (req, res) => {
