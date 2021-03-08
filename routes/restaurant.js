71664b197420aef29bfe8176fb0c111e4d2be876
jwilson42 <wilson.jd022@gmail.com>
2021-03-08 12:04:51 -0800

    josh: added a route to return all restaurants

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -3,8 +3,9 @@ const ctrl = require("../controllers");
 const passport = require("passport");
 
 router.get("/test", ctrl.retaurant.test);
+router.get("/all", ctrl.retaurant.all);
+router.get("/:id", ctrl.retaurant.profile);
 router.post("/register", ctrl.retaurant.register);
 router.post("/login", ctrl.retaurant.login);
-router.get("/:id", ctrl.retaurant.profile);
 
 module.exports = router;
