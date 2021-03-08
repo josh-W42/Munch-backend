f63c80a4d9088093345a50bd3e6a181570498e21
Andrew8bit <Andrewbith@gmail.com>
2021-03-08 09:41:38 -0800

    created categories in seed.js

--- seed.js
+++ seed.js
@@ -1,23 +1,35 @@
 const db = require('./models');
 
-const examples = [
-  { name: 'Example 1', completed: true}, 
-  { body: 'Example 2', completed: false}
+const categories = [ // multiple categories
+  { name: 'Breakfast'},
+  { name: 'Brunch'}, 
+  { name: 'Lunch'},
+  { name: 'Dinner'},
+  { name: 'Ramen'},
+  { name: 'Japanese'}, 
+  { name: 'Thai'},
+  { name: 'Mexican'},
+  { name: 'Vegetarian'},
+  { name: 'Vegan'}, 
+  { name: 'Halal'},
+  { name: 'Mediterranean'},
+  { name: 'Italian'},
 ];
-const oneExample = { name: 'Example 3', completed: true };
 
-const addManyExamples = async () => {
-  const savedExamples = await db.Example.insertMany(examples);
-  console.log('=======> Saved Examples.');
+const oneCategory = { name: 'Chinese'}; // single category
+
+const addManyCategories = async () => {
+  const savedExamples = await db.Example.insertMany(examples); // creating many categories
+  console.log('=======> Saved Many Categories.');
   console.log(savedExamples);
 }
 
-const addOneExample = async () => {
-  const savedOneExample = await db.Example.create(oneExample);
-  console.log("=======> Saved One Example.");
+const addOneCategory = async () => {
+  const savedOneExample = await db.Example.create(oneExample); // creating the one category
+  console.log("=======> Saved One Category.");
   console.log(savedOneExample);
 };
 
 // run the functions
-addManyExamples();
-addOneExample();
+addManyCategories();
+addOneCategory();
