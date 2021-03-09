193d1d244be2d7c1b868558c9fe4fb3311acefd3
Andrew8bit <Andrewbith@gmail.com>
2021-03-09 10:53:03 -0800

    Andrew:  added an edit post route - auth tested - functionality tested

--- routes/post.js
+++ routes/post.js
@@ -3,15 +3,15 @@ const ctrl = require("../controllers");
 const passport = require("passport");
 
 router.get("/test", ctrl.post.test);
-router.get("/:id", ctrl.post.showPost)
 router.get("/index", ctrl.post.postIndex);
+router.get("/:id", ctrl.post.showPost)
 router.get("/index/author/:id", ctrl.post.postIndexAuthor);
 router.get("/index/restaurant/:id", ctrl.post.postIndexRestaurant);
 
 router.post("/createPost/:id/", passport.authenticate('jwt', { session: false }), ctrl.post.createPost);
 router.post("/addComment/:id/", passport.authenticate('jwt', { session: false }), ctrl.post.addNewComment);
 
-router.put("/:id/edit", passport.authenticate('jwt', { session: false }), ctrl.post.updatePost);
+router.put("/:id/update", passport.authenticate('jwt', { session: false }), ctrl.post.updatePost);
 
 router.delete("/:id/delete", passport.authenticate('jwt', { session: false }), ctrl.post.deletePost)
 
