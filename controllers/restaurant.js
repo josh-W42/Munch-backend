ad9baf303d04c7fad18ccd75012ed3c460a7e8e9
jwilson42 <wilson.jd022@gmail.com>
2021-03-11 09:56:48 -0800

    josh: getting rid of unused dependancies

--- controllers/restaurant.js
+++ controllers/restaurant.js
@@ -1,6 +1,5 @@
 // imports
 require("dotenv").config();
-const passport = require("passport");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = process.env;
